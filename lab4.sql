9a19298c2f089f35c9414ca57184e82a217c9da1
carsonbryant21 <bryantcj@dukes.jmu.edu>
2024-10-07 22:36:16 -0400

    Create lab4.sql

---
+++ lab4.sql
@@ -0,0 +1,21 @@
+--Q4.1
+/*No, because we still have the virginia records in the other tables. */
+
+--Q4.2
+/* No, because there are no fips when the value is 80 */
+
+--Q4.3
+/* No. */
+
+--Q4.5
+/* Yes */
+
+--Q4.6
+/* Yes */
+
+--Q4.7
+/* The prompt is "calculate the year-over-year population growth rate for the VA state in the recent 5 years." We revised the prompt to include the table names and the filed names from the diagram. */
+
+--Q4.8
+/* No, it does not always produce accurate results. */
+
